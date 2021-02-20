1c26f2fe1a7b83cc2da2713a480658691042a7ae
Abbas Salami <olawalesalami3@gmail.com>
2021-02-20 18:33:09 +0100

    setup dashboard double-nav

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {  
+    // SideNav Initializationa
+      $(".button-collapse").sideNav({
+        breakpoint: 1100, // Breakpoint for button collapse
+        menuWidth: 200, // Width for sidenav
+        showOverlay: true, // Display overflay
+    });
+});
